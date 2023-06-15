0208e539fbca429cd6530d5780018d44bdd44c5c
Zach <vanfo010@umn.edu>
2023-06-15 10:39:17 -0500

    changes

--- EuTiO3-scripts/linear-regression-exchange-constanst.py
+++ EuTiO3-scripts/linear-regression-exchange-constanst.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_csv('EuTiO3-exchange-counting.csv')
-exchange_coefficients = data[['J100 coeff', 'J110 coeff', 'J111 coeff']]
+exchange_coefficients = data[['J100 coeff', 'J110 coeff', 'J111 coeff']].to_numpy()
+free_energy = data[['free energy']].to_numpy()
 
-"test"
+model = LinearRegression().fit(exchange_coefficients, free_energy)
+params = model.coef_
+
+print(params)
