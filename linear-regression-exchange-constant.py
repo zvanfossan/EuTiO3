9fae64c3d9d2074a52bd74829b9d318157371a77
Zach <vanfo010@umn.edu>
2023-06-21 15:25:16 -0500

    update

---
+++ linear-regression-exchange-constant.py
@@ -0,0 +1,38 @@
+"""Calculating exchange constants for EuTiO3"""
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('EuTiO3-exchange-counting.csv')
+data_1 = data.drop(labels=[11], axis=0)
+exchange_coefficients = data_1[['J100 coeff double', 'J110 coeff double', 
+                                'J111 coeff double', 'e0 coeff']].to_numpy()
+free_energy = data_1['free energy'].to_numpy()
+free_energy_offset = np.zeros(shape=(len(free_energy)))
+estimate_offset = np.zeros(shape=(len(free_energy)))
+
+model = LinearRegression(fit_intercept=False).fit(exchange_coefficients, free_energy)
+params = model.coef_
+intercept = model.intercept_
+estimate = model.predict(exchange_coefficients)
+
+line_x = np.array([-1000,1000])
+line_y = np.array([-1000,1000])
+
+for i in range(len(free_energy_offset)):
+    free_energy_offset[i] = (free_energy[i] - free_energy[len(free_energy)-1])*10**3
+    estimate_offset[i] = (estimate[i] - estimate[len(estimate)-1])*10**3
+
+plt.plot(free_energy_offset, estimate_offset, linestyle = 'none', marker = 'o')
+plt.plot(line_x,line_y,linestyle = 'dashed', linewidth = '1', color = 'black')
+plt.ticklabel_format(style='sci', useOffset=False)
+plt.xlim(-0.4,15)
+plt.ylim(-0.4,15)
+plt.xlabel("DFT calculated energy (meV)")
+plt.ylabel("Linear regression prediction (meV)")
+#plt.show()
+
+print('\n------------------------------------------\n')
+print("Model Parameters:", params)
+print('\n------------------------------------------')
