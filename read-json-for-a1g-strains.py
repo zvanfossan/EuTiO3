e97bc06bd94a40e39c6e5130da7a690ce02bffe9
Zach <vanfo010@umn.edu>
2023-08-07 14:52:20 -0500

    updates

--- read-json-for-a1g-strains.py
+++ read-json-for-a1g-strains.py
@@ -20,25 +20,26 @@ def read_json(fjson):
         with open(fjson) as f:
             return json.load(f)
 
-dict = read_json('data/a0a0a0_gm3_0a_free_energy_for_different_magnetic_configurations/free-energy-data.json')
+dict = read_json('data/a0a0a0_a1g_strain_free_energy_for_different_magnetic_configurations_and_strains/free-energy-data.json')
 df = pd.DataFrame.from_dict(dict, orient='columns')
 
 nearest_neigbors_df = pd.read_excel('data/compiled-nearest-neighbor-counting-for-octahedral-rotations.xlsx')
 #cubic_df = pd.read_excel('data/EuTiO3-exchange-counting-cubic.xlsx') #add column from separate file for cubic phase free energies
 
-def convert_data_frame_values_to_floats_and_orient_rows(df):
-      for column in df:
-            for i in range(len(df[column])):
+for column in df:
+      for i, item in enumerate(df[column]):                
                   df[column][i] = df[column][i]['free energy']
-      desired_row_order = ['1_atom','2_atom_100','2_atom_110','2_atom_111','3_atom_100_110',
-                       '3_atom_100_111','3_atom_101_110','4_atom_1','4_atom_2','4_atom_3',
-                       '4_atom_4','4_atom_5','4_atom_6','ferro']
-      df = df.reindex(desired_row_order)
-      return df
-df = convert_data_frame_values_to_floats_and_orient_rows(df)
+
+            #df[column][i] = df[column][item["free energy"]]
+
+desired_row_order = ['1_atom','2_atom_100','2_atom_110','2_atom_111','3_atom_100_110',
+                 '3_atom_100_111','3_atom_101_110','4_atom_1','4_atom_2','4_atom_3',
+                 '4_atom_4','4_atom_5','4_atom_6','ferro']
+df = df.reindex(desired_row_order)
+
 
 desired_column_order = sorted([float(col) for col in df.columns])
-desired_column_order[0] = format(desired_column_order[0], '.0f')
+desired_column_order[10] = format(desired_column_order[10], '.0f')
 desired_column_order = [str(value) for value in desired_column_order]
 print(desired_column_order)
 df = df[desired_column_order]
@@ -49,6 +50,8 @@ print(df)
 print(nearest_neigbors_df.head())
 
 #now perform multiple linear regression using the structured datasets
+scores = []
+paramagnetic = []
 j1xy = []
 j1z = []
 j2xy = []
@@ -101,7 +104,9 @@ for strains in df.columns.tolist():
       j2zerror.append(standard_error[3])
       j3error.append(standard_error[4])
       strain.append(strains)
-
+      scores.append(score)
+      paramagnetic.append(paramagnetic_energy
+                          )
       #parity plot data formatting 
       minimum_energy_index = np.where(y == min(y))[0][0]
       free_energy_offset = np.zeros(shape=len(y))
@@ -121,18 +126,37 @@ for strains in df.columns.tolist():
       #plt.ylabel("Linear regression prediction (meV)")
       #plt.show()
 
-plt.errorbar(strain, j1xy, yerr=j1xyerror, capsize=2, linewidth = 2)
-plt.errorbar(strain, j1z, yerr=j1zerror, capsize=2, linewidth = 2)
-plt.errorbar(strain, j2xy, yerr=j2xyerror, capsize=2, linewidth = 2)
-plt.errorbar(strain, j2z, yerr=j2zerror, capsize=2, linewidth = 2)
-plt.errorbar(strain, j3, yerr=j3error, capsize=2, linewidth = 2)
+#plt.errorbar(strain, j1xy, yerr=j1xyerror, capsize=2, linewidth = 2)
+#plt.errorbar(strain, j1z, yerr=j1zerror, capsize=2, linewidth = 2)
+#plt.errorbar(strain, j2xy, yerr=j2xyerror, capsize=2, linewidth = 2)
+#plt.errorbar(strain, j2z, yerr=j2zerror, capsize=2, linewidth = 2)
+#plt.errorbar(strain, j3, yerr=j3error, capsize=2, linewidth = 2)
 #plt.vlines(x = 5.77,ymin=-1e-4,ymax=1e-4,color = 'black', ls='--')
 #plt.ylim(-1e-5,3e-5)
-plt.legend(['J1 x-y','J1 z','J2 x-y','J2 z', 'J3','relaxed structure angle'], fontsize = 12)
-plt.xlabel('Strain %')
-plt.ylabel('Exchange value')
-plt.show()
+#plt.legend(['J1 x-y','J1 z','J2 x-y','J2 z', 'J3','relaxed structure angle'], fontsize = 12)
+#plt.xlabel('Strain %')
+#plt.ylabel('Exchange value')
+#plt.show()
 
+data = {"strains" : strain, 
+        "exchanges" : {"j1xy" : j1xy, "j1z" : j1z,
+                        "j2xy" : j2xy, "j2z" : j2z,
+                       "j3" : j3, "paramagnetic energy" : paramagnetic,
+                       "score" : scores},
+        "error" : {"j1xyerror" : j1xyerror, "j1zerror" : j1zerror, 
+                   "j2xyerror" : j2xyerror, "j2zerror" : j2zerror,
+                   "j3error" : j3error}
+      }
+
+DATA_DIR = './'
+fjson = os.path.join(DATA_DIR, "a1g-exchange-data.json")
+
+def write_json(d,fjson):
+        with open(fjson, "w") as f:
+                json.dump(d,f)
+        return d
+
+write_json(data, fjson)
 
 
 
